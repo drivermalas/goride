25566b456132973e9fbf9f28b6d30f1271914634
drivermalas <60193017+drivermalas@users.noreply.github.com>
2020-03-11 12:59:32 +0700

    Update goride.php

--- goride.php
+++ goride.php
@@ -10,7 +10,7 @@ $header[] = "X-AppVersion: 3.46.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -3.595195,98.672222";
+$header[] = "X-Location: -6.170186,106.824174";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
