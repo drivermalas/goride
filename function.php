cc8db48f0bd1dad7bd75a4c08bf807ea94aded2b
drivermalas <60193017+drivermalas@users.noreply.github.com>
2020-04-03 17:33:02 +0700

    Update function.php

--- function.php
+++ function.php
@@ -9,7 +9,7 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.917464,107.619122";
+$header[] = "X-Location: -6.220".mt_rand(100,999).",106.77".mt_rand(1000,9999);
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
