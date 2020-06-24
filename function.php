ff36be87a0fff8f7642ccb8ae2808afc58880ac8
drivermalas <60193017+drivermalas@users.noreply.github.com>
2020-06-24 19:48:34 +0700

    Update function.php

--- function.php
+++ function.php
@@ -7,11 +7,11 @@ $header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: id-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.55.4";
+$header[] = "X-AppVersion: 3.52.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.870168,107.619122";
+$header[] = "X-Location: -6.957858,110.438125";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
